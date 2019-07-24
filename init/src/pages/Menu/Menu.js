608a59f02d65fd38575ea6ac7a61457920da854b
demo <1581649460@qq.com>
2019-07-24 22:15:12 +0800

    ss1

---
+++ init/src/pages/Menu/Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./index.less"
+export default class Menu extends Component {
+    render() {
+        return (
+            <div className="menu">
+                我是首页
+            </div>
+        )
+    }
+}
